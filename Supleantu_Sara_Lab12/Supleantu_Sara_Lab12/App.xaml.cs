4de5dfb5c75ed12f46e8fdcad84296c859562db3
SARA-MARIA SUPLEANTU <sara.supleantu@stud.ubbcluj.ro>
2021-01-05 16:36:57 +0200

    Add project files.

---
+++ Supleantu_Sara_Lab12/Supleantu_Sara_Lab12/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Supleantu_Sara_Lab12.Data;
+
+namespace Supleantu_Sara_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
